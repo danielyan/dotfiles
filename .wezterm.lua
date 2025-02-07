be62f035f9b49b9bd647ae90d3dcdff280d5d5b1
Lev Danielyan <lev@danielyan.org>
2025-02-06 17:59:25 -0800

    wezterm opacity and size

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,4 +4,13 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Obsidian'
 config.font = wezterm.font 'Fira Code'
+
+config.font_size = 13
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.90
+config.macos_window_background_blur = 10
+config.initial_rows = 30
+config.initial_cols = 120
+
 return config
