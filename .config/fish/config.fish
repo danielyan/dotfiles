1435ab655fbae0a6294f1da34f9311e34194c504
Lev Danielyan <lev@danielyan.org>
2025-02-13 19:57:31 -0800

    Moarr updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,3 +24,6 @@ abbr --add gl git lg
 abbr --add gd git diff
 abbr --add gp git pull
 abbr --add gpp git push
+
+#------ misc
+abbr --add l ls -la --color | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(" %0o ",k);print}'
